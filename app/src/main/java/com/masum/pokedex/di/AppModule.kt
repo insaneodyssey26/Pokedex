962f67e8724f93955b6f3e78d19e4fb348f07c58
Masum <hydrapikachu29@gmail.com>
2025-07-06 17:06:01 +0530

    Retrofit builder

--- app/src/main/java/com/masum/pokedex/di/AppModule.kt
+++ app/src/main/java/com/masum/pokedex/di/AppModule.kt
@@ -1,10 +1,30 @@
 package com.masum.pokedex.di
 
+import com.masum.pokedex.data.remote.responses.PokeAPI
+import com.masum.pokedex.repository.PokemonRepository
+import com.masum.pokedex.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    fun providePokemonRepository(
+        api: PokeAPI
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun pokeApi() : PokeAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeAPI::class.java)
+    }
 }
---
+++ app/src/main/java/com/masum/pokedex/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.masum.pokedex.util
+
+object Constants {
+
+    const val  BASE_URL = "https://pokeapi.co/api/v2/"
+}
