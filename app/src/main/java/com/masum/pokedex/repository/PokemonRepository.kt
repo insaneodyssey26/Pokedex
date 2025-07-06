311a179bee7310c34a562de55446b24f13116338
Masum <hydrapikachu29@gmail.com>
2025-07-06 16:57:56 +0530

    Error handling for pokemon info

--- app/src/main/java/com/masum/pokedex/repository/PokemonRepository.kt
+++ app/src/main/java/com/masum/pokedex/repository/PokemonRepository.kt
@@ -2,6 +2,7 @@ package com.masum.pokedex.repository
 
 import androidx.compose.ui.geometry.Offset
 import com.masum.pokedex.data.remote.responses.PokeAPI
+import com.masum.pokedex.data.remote.responses.Pokemon
 import com.masum.pokedex.data.remote.responses.PokemonList
 import com.masum.pokedex.util.Resource
 import dagger.hilt.android.scopes.ActivityScoped
@@ -14,6 +15,15 @@ class PokemonRepository @Inject constructor(
     suspend fun PokemonList(limit: Int, offset: Int) : Resource<PokemonList> {
         val response = try {
             api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error ("Unknown error occurred" )
+        }
+        return Resource.Success(response)
+    }
+
+    suspend fun PokemonInfo(pokemonName: String) : Resource<Pokemon> {
+        val response = try {
+            api.getPokemonDetails(pokemonName)
         } catch (e: Exception) {
             return Resource.Error ("Unknown error occurred" )
         }
