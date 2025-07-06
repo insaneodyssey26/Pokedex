08a6702805d14baf73bf854e894a0bbbc77e087b
Masum <hydrapikachu29@gmail.com>
2025-07-07 00:57:55 +0530

    Search bar

--- app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListScreen.kt
@@ -1,17 +1,31 @@
 package com.masum.pokedex.pokemonlist
 
+import android.icu.text.StringSearch
+import android.telecom.StatusHints
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.masum.pokedex.R
@@ -34,5 +48,40 @@ fun PokemonListScreen(
                     .align(Alignment.CenterHorizontally)
             )
         }
+    }
+}
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onSearch: (String) -> Unit = {},
+) {
+    var text by remember {
+        mutableStateOf("")
+    }
+    var isHintVisible by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = Modifier) {
+        BasicTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                onSearch(it)
+            },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White,CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged{
+                    isHintVisible = ! it.isFocused
+                }
+        )
     }
 }
