ba7027e39c8053f5944b562cd0a1b66de7bc27fa
Masum <hydrapikachu29@gmail.com>
2025-07-11 19:22:39 +0530

    Page size constant added

--- app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -6,11 +6,14 @@ import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.masum.pokedex.repository.PokemonRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import androidx.palette.graphics.Palette
 import com.masum.pokedex.data.models.PokedexListEntry
+import com.masum.pokedex.util.Constants.PAGE_SIZE
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
@@ -22,6 +25,12 @@ class PokemonListViewModel @Inject constructor(
     val loadError = mutableStateOf("")
     val isLoading = mutableStateOf(false)
     val endReached = mutableStateOf(false)
+
+    fun loadPokemonPaginated() {
+        viewModelScope.launch {
+            val result = repository.PokemonList(PAGE_SIZE, currPage * PAGE_SIZE)
+        }
+    }
     fun DominantColor (drawable: Drawable, onFinish: (Color) -> Unit) {
         val bmap = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
 
