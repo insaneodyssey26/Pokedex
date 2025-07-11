93729f30ea964e4737d97a41d83a6b8f3494b35c
Masum <hydrapikachu29@gmail.com>
2025-07-11 20:05:09 +0530

    Used java locale instead of compose

--- app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/masum/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -6,7 +6,6 @@ import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.capitalize
-import androidx.compose.ui.text.intl.Locale
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.masum.pokedex.repository.PokemonRepository
@@ -42,7 +41,7 @@ class PokemonListViewModel @Inject constructor(
                             entry.url.takeLastWhile { it.isDigit() }
                         }
                         val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
-                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                        PokedexListEntry(entry.name.capitalize(java.util.Locale.ROOT), url, number.toInt())
                     }
                 }
                 is Resource.Error -> {
