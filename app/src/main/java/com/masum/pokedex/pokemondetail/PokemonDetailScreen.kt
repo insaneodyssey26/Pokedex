9d86b06f0e1be28856d97f8a1ae1774fe2095e2d
Masum <hydrapikachu29@gmail.com>
2025-07-13 21:25:33 +0530

    Animation properties

--- app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -1,6 +1,8 @@
 package com.masum.pokedex.pokemondetail
 
 import android.text.Layout
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -26,8 +28,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -309,11 +312,25 @@ fun PokemonDetailDataItem(
 fun PokemonStat (
     statName: String,
     statValue: Int,
-    statMaxValue: Int
+    statMaxValue: Int,
     statColor: Color,
     height: Dp = 28.dp,
     animDuration: Int = 1000,
     animDelay: Int = 0
 ) {
-
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currPercent = animateFloatAsState(
+        targetValue = if (animationPlayed) {
+        statValue / statMaxValue.toFloat()
+        } else 0f,
+        animationSpec = tween(
+            animDuration,
+            animDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
 }
