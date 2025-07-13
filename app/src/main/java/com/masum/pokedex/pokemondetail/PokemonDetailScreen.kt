4a1183163b6eb52f9ffc424f6208ae2602609e51
Masum <hydrapikachu29@gmail.com>
2025-07-13 17:40:54 +0530

    Pokemons parsed and type section

--- app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -35,13 +38,17 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.masum.pokedex.data.remote.responses.Pokemon
+import com.masum.pokedex.data.remote.responses.Type
 import com.masum.pokedex.util.Resource
+import com.masum.pokedex.util.parseTypeToColor
 import java.nio.file.WatchEvent
+import java.sql.Types
 import java.util.Locale
 
 @Composable
@@ -195,5 +202,32 @@ fun PokemonDetailSection(
             textAlign = TextAlign.Center,
             color = MaterialTheme.colorScheme.onSurface
         )
+    }
+}
+
+@Composable
+fun PokemonTypeSection(types: List<Type>) {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(16.dp)
+    ){
+        for (type in types) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp)
+                    .clip(CircleShape)
+                    .background(parseTypeToColor(type))
+                    .height(35.dp)
+            ) {
+                Text(
+                    text = type.type.name.capitalize(Locale.ROOT),
+                    color = Color.White,
+                    fontSize = 18.sp
+                )
+            }
+        }
     }
 }
