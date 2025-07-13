abf18c21b009eb67d5267bc80bf4c9b4d82b19a0
Masum <hydrapikachu29@gmail.com>
2025-07-13 13:50:08 +0530

    Top section

--- app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -1,15 +1,20 @@
 package com.masum.pokedex.pokemondetail
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
@@ -59,5 +64,36 @@ fun PokemonDetailScreen(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PokemonDetailTopSection(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.TopCenter,
+        modifier = Modifier
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        Color.Black,
+                        Color.Transparent
+                    )
+                )
+            )
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = null,
+            tint = Color.White,
+            modifier = Modifier
+                .size(36.dp)
+                .offset(16.dp, 16.dp)
+                .clickable {
+                    navController.popBackStack()
+                }
+        )
     }
 }
