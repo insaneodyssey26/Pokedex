88602ad03d7813de8fa63d31eecce5d4fbdc0136
Masum <hydrapikachu29@gmail.com>
2025-07-13 13:27:45 +0530

    Image nightmare

--- app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/masum/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -3,15 +3,21 @@ package com.masum.pokedex.pokemondetail
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.masum.pokedex.data.remote.responses.Pokemon
 import com.masum.pokedex.util.Resource
 
@@ -24,14 +30,34 @@ fun PokemonDetailScreen(
     pokemonImageSize: Dp = 200.dp,
     viewModel: PokemonDetailViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
     val PokemonInfo = produceState<Resource<Pokemon>>(initialValue = Resource.Loading<Pokemon>()) {
         value = viewModel.getPokemonInfo(pokemonName)
-    }
+    }.value
     Box(modifier = Modifier
         .fillMaxSize()
         .background(dominantColor)
         .padding(bottom = 16.dp)
     ) {
-
+        Box (
+            contentAlignment = Alignment.TopCenter,
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            if (PokemonInfo is Resource.Success) {
+                PokemonInfo.data?.sprites?.let {
+                    AsyncImage(
+                        model = ImageRequest.Builder(context)
+                            .data(it.front_default)
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = PokemonInfo.data.name,
+                        modifier = Modifier
+                            .size(pokemonImageSize)
+                            .offset(y = topPadding)
+                    )
+                }
+            }
+        }
     }
 }
